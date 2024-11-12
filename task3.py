fafaabd70e969486e50b2feb8b53742a512ff6c3
Boryslaw Humenuik <boryshumm@gmail.com>
2024-11-12 11:01:11 +0200

    task3.py

--- task3.py
+++ task3.py
@@ -1,23 +1,20 @@
-lst = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда'] 
-lst_int = [] 
-lst_str = [] 
- 
-def cln_lst(lst): 
-    return list(set(lst)) 
-cleaned_lst = cln_lst(lst) 
- 
-def sort_lst(lst): 
-    for item in lst: 
-        if type(item) == int: 
-            lst_int.append(item) 
-        if type(item) == str: 
-            item = item.lower() 
-            lst_str.append(item) 
- 
-sort_lst(cleaned_lst) 
- 
-lst_int.sort() 
-lst_str.sort() 
- 
-lst = lst_int + lst_str 
-print(lst)
+lst = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
+cleaned_lst = clean_list(lst)
+sorted_lst = sort_list(cleaned_lst)
+def clean_list(lst)
+    return list(set(lst))
+
+def sort_list(lst):
+    lst_int = []
+    lst_str = []
+    for item in lst:
+        if isinstance(item, int):
+            lst_int.append(item)
+        elif isinstance(item, str):
+            lst_str.append(item.lower())
+    lst_int.sort()
+    lst_str.sort()
+   
+    return lst_int + lst_str
+
+print(sorted_lst)
